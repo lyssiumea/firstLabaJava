96bec53ac919ca11d4f9fd2b03326b39b1285c7e
alyssa <lyssiumea@mail.ru>
2024-10-14 22:41:46 +0500

    14/20

--- src/Cycles.java
+++ src/Cycles.java
@@ -44,5 +44,15 @@ public class Cycles {   // Задание 3. Циклы
         return true;
     }
 
+    // Задача № 8. Левый треугольник.
+    public void leftTriangle(int x) {
+        for (int i = 1; i <= x; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
 
 }
