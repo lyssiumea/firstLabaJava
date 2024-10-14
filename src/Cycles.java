45aabda844d3f1440c286ac16b6fcc35b7bdc91b
alyssa <lyssiumea@mail.ru>
2024-10-14 22:37:32 +0500

    Revert "14/20"
    This reverts commit c1c612f5

--- src/Cycles.java
+++ src/Cycles.java
@@ -46,6 +46,10 @@ public class Cycles {   // Задание 3. Циклы
 
     // Задача № 8. Левый треугольник.
     public void leftTriangle(int x) {
+        if (x <= 0) {
+            System.out.println("Ошибка: введите положительное число для высоты треугольника.");
+            return;
+        }
         for (int i = 1; i <= x; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
